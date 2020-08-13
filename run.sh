67cd15f3cd759ca2cf256e32435e9aefe38c06da
Mattia <mattiafailla@hotmail.it>
2020-08-13 16:47:58 +0200

    Improving cache

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gmc --syncmode "fast" --cache="2048" --maxpeers="128" --etherbase "0xa16497d81206c21dd86e67467733c0820359e2fe" --rpc --rpcaddr "0.0.0.0" --rpcport 8545 --identity "NextBlu-musicoin-pool" --metrics --ethstats="NextBlu:musicstatsbypirl@stats.musicoin.org"
+gmc --syncmode "fast" --cache="4096" --maxpeers="128" --etherbase "0xa16497d81206c21dd86e67467733c0820359e2fe" --rpc --rpcaddr "0.0.0.0" --rpcport 8545 --identity "NextBlu-musicoin-pool" --metrics --ethstats="NextBlu:musicstatsbypirl@stats.musicoin.org"
