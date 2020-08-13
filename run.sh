8dd4ad53d6e54773920cfa8b64422bc074f8e53f
Mattia <mattiafailla@hotmail.it>
2020-08-13 09:26:39 +0200

    Setting pool identity

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gmc --fast --etherbase "0xa16497d81206c21dd86e67467733c0820359e2fe" --rpc --rpcaddr "0.0.0.0" --rpcport 8545
+gmc --fast --etherbase "0xa16497d81206c21dd86e67467733c0820359e2fe" --rpc --rpcaddr "0.0.0.0" --rpcport 8545 --identity "NextBlu-musicoin-pool"
